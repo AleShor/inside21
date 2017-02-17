c2edd66c905e9ec05959ee24969903fb861cd00f
Aleksey Shornikov <aleshordev@gmail.com>
2017-02-17 18:29:32 +0300

    update: add faq section

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
 $(document).ready(function(){
 	createPriceList();
-
-	function createPriceList() {
-		var $priceList = $('#prices-list');
-
-		priceList.forEach(function(price) {			
-			var srvcs = '';
-			price.services.forEach(function(srvc){
-				srvcs += '<li>' + srvc + '</li>';
-			});
-
-			$priceList.append(
-				'<div class="price-wrapper">'
-					+'<li class="price-box">'
-						+'<div class="price-img">'
-							+'<img src="img/' + price.image + '">'
-						+'</div>'
-
-						+'<ul class="price-services">'
-							+ srvcs
-						+'</ul>'
-
-						+'<div class="price-cost">'
-							+'<p>от ' + price.cost + ' руб. за км2</p>'
-						+'</div>'
-
-						+'<button class="price-btn btn">'
-							+'Оставить заявку'
-						+'</button>'
-					+'</li>'
-				+'</div>'
-			);
-		})
-	}
-
+	createFaqList();
 });
 
 
+
+/*----------------PRICE LIST----------------------------------------------*/
+function createPriceList() {
+	var $priceList = $('#prices-list');
+
+	priceList.forEach(function(price) {			
+		var srvcs = '';
+		price.services.forEach(function(srvc){
+			srvcs += '<li>' + srvc + '</li>';
+		});
+
+		$priceList.append(
+			'<div class="price-wrapper">'
+				+'<li class="price-box">'
+					+'<div class="price-img">'
+						+'<img src="img/' + price.image + '">'
+					+'</div>'
+
+					+'<ul class="price-services">'
+						+ srvcs
+					+'</ul>'
+
+					+'<div class="price-cost">'
+						+'<p>от ' + price.cost + ' руб. за км2</p>'
+					+'</div>'
+
+					+'<button class="price-btn btn">'
+						+'Оставить заявку'
+					+'</button>'
+				+'</li>'
+			+'</div>'
+		);
+	})
+}
 
 var priceList = [
 	{
@@ -113,3 +114,47 @@ var priceList = [
 		]
 	},
 ];
+
+
+/*----------------FAQ LIST----------------------------------------------*/
+
+function createFaqList() {
+	var $faqList = $('#faq-list');
+
+	faqList.forEach(function(faq, indx) {
+		var srvcs = '';
+
+		$faqList.append(
+			'<div class="faq-item">'
+				+'<h1 class="faq-number">' + (indx+1) + '</h2>'
+				+'<h3 class="faq-question">' + faq.qstn + '</h3>'
+				+'<p class="faq-answer">' + faq.answr + '</p>'
+			+'</div>'
+		);
+	});
+}
+
+
+var faqList = [
+	{
+		qstn: 'Могу ли я доверить вам свою квартиру?',
+		answr: 'ООО "Инсайд" на рынке строительных услуг более 10 лет и за это время не было ни одного недовольного клиента.' 
+					+' Все работы ведутся с соблюдением всех технологий.',
+	},
+
+	{
+		qstn: 'Надеюсь вы качественно делаете свою работу?',
+		answr: 'Качество обеспечивается контрактом. Также мы предоставляем гарантию 1 год на всю проделанную работу.',
+	},
+
+	{
+		qstn: 'Нужно ли мне покупать материалы и заботиться о мусоре?',
+		answr: 'Нет, мы выполняем работу под ключ: закупку и доставку материалов берем на себя. На объекте гарантируется чистота. Мусор тоже вывозим сами.',
+	},
+
+	{
+		qstn: 'Кто будет непосредственно выполнять ремонт?',
+		answr: 'У нас свой штат строителей. Все рабочие - квалифицированные специалисты',
+	},
+];
+
